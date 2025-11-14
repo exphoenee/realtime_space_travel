764704947bafe7d198846c8acb9344eca24f369e
exphoenee <bozzay.viktor@gmail.com>
2025-11-14 12:26:14 +0100

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/urutazas-szimulator/', // << FONTOS: Cseréld le ezt a saját repository-d nevére!
+  base: "/realtime_space_travel/", // << FONTOS: Cseréld le ezt a saját repository-d nevére!
 });
