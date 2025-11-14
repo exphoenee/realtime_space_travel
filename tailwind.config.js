e6114869cbb79aceab63bcf9958c88e9595c65ca
exphoenee <bozzay.viktor@gmail.com>
2025-11-14 12:16:25 +0100

    fiexd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./{App,components/*}.tsx",
-  ],
+  content: ["./index.html", "./**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
